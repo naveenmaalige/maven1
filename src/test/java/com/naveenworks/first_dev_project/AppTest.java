12809e2533bd4f403df7a0ff99f8e0aaa8487348
Naveen <naveenmaalige@gmail.com>
2022-07-21 14:02:22 +0530

    initial commits

---
+++ src/test/java/com/naveenworks/first_dev_project/AppTest.java
@@ -0,0 +1,20 @@
+package com.naveenworks.first_dev_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
